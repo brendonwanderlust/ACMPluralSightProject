aca7eaa006039695758f5ba410ef6cd17007e96d
Brendon Carrasquillo <33206680+brendonwanderlust@users.noreply.github.com>
2019-05-02 18:06:45 -0400

    commit

--- ACM.BL/ProductRepository.cs
+++ ACM.BL/ProductRepository.cs
@@ -18,8 +18,12 @@ namespace ACM.BL
                 product.ProductDescription = "Assorted Size Set of 4 Bright Yellow Mini Sunflowers";
                 product.CurrentPrice = 15.96M;
             }
+
+            
             return product;
         }
+
+
 
         public bool Save(Product product)
         {
